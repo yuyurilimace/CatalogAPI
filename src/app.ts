3c5fce4d47f86cd05ba2178337431b5e36c9c729
José Yuri Lima Lira <yurilima.ce@gmail.com>
2024-07-17 16:31:18 -0300

    feat:add dotenv to work with envoriment variables, and add prisma ORM to project

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import { app } from "./expressConfig";
+import { PrismaClient } from "@prisma/client";
+import "dotenv/config";
 const port = 3001;
+const prismaClient = new PrismaClient();
 
 const StartCatalogApi = () => {
-  app.listen(port, () => {
+  app.listen(process.env.PORT, () => {
     console.log(`API Listening on port ${port}`);
   });
 };
