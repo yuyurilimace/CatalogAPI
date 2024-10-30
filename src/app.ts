63f6dc2a5fb20b551820d7c0a0712861a844c56f
José Yuri Lima Lira <yurilima.ce@gmail.com>
2024-10-30 08:46:10 -0300

    feat: adds user profile routes

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import { app, jsonBodyConfig } from "./expressConfig";
 import { PrismaClient } from "@prisma/client";
 import "dotenv/config";
 import { UserRoutes } from "./routes/userRoutes";
+import { ProfileRoutes } from "./routes/profileRoutes";
 
 const port = 3001;
 const prismaClient = new PrismaClient();
@@ -10,6 +11,8 @@ const StartCatalogApi = () => {
   app.use(jsonBodyConfig);
 
   app.use("/user", UserRoutes());
+  app.use("/profile", ProfileRoutes());
+
   app.listen(process.env.PORT, () => {
     console.log(`API Listening on port ${port}`);
   });
---
+++ src/routes/profileRoutes.ts
@@ -0,0 +1,49 @@
+import { Router } from "express";
+import { HandleAsyncFunction } from "../utils/handleErros";
+import { ProfileDTO } from "../DTO/ProfileDTO";
+import { ProfileDomain } from "../domain/profileDomain";
+import { HTTPStatusCode } from "../enuns/response";
+
+const ProfileRoutes = () => {
+  const router = Router();
+
+  router.post(
+    "/create",
+    HandleAsyncFunction<ProfileDTO>(
+      (profile: ProfileDTO) => ProfileDomain.CreateNewProfile(profile.type),
+      HTTPStatusCode.Created,
+      "Perfil criado com  sucesso"
+    )
+  );
+
+  router.put(
+    "/update",
+    HandleAsyncFunction<ProfileDTO>(
+      (profile: ProfileDTO) => ProfileDomain.UpdateProfile(profile),
+      HTTPStatusCode.Ok,
+      "Perfil editado com  sucesso"
+    )
+  );
+
+  router.delete(
+    "/",
+    HandleAsyncFunction<ProfileDTO>(
+      (profile: ProfileDTO) => ProfileDomain.DeleteProfile(profile.id),
+      HTTPStatusCode.Ok,
+      "Perfil apagado com sucesso"
+    )
+  );
+
+  router.get(
+    "/",
+    HandleAsyncFunction<ProfileDTO>(
+      (profile: ProfileDTO) => ProfileDomain.GetProfile(profile.id),
+      HTTPStatusCode.Ok,
+      "Perfil obtido com sucesso"
+    )
+  );
+
+  return router;
+};
+
+export { ProfileRoutes };
